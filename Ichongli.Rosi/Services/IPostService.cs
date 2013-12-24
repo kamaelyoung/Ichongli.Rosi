f7d6cce700a92ab0710288543213848b9e5f413c
kamaelyoung <kamaelyoung@live.com>
2013-12-25 00:10:05 +0800

    IPostService

---
+++ Ichongli.Rosi/Services/IPostService.cs
@@ -0,0 +1,14 @@
+﻿using Ichongli.Rosi.Entitys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ichongli.Rosi.Services
+{
+    public interface IPostService
+    {
+        Task<List<PostItem>> get_recent_posts(string url);
+    }
+}
---
+++ Ichongli.Rosi/Services/PostService.cs
@@ -0,0 +1,19 @@
+﻿namespace Ichongli.Rosi.Services
+{
+    using Ichongli.Rosi.Entitys;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class PostService : IPostService
+    {
+        public async Task<List<PostItem>> get_recent_posts(string url)
+        {
+            var result = new List<PostItem>();
+
+            return result;
+        }
+    }
+}
