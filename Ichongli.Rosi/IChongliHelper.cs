9cf7c369d0b4af887398476b2ea9da248700da80
kamaelyoung <kamaelyoung@live.com>
2013-12-25 11:03:51 +0800

    Add Ninject

---
+++ Ichongli.Rosi/IChongliHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ichongli.Rosi
+{
+    public static class IChongliHelper
+    {
+        private const string baseUrl = "http://rosimm.ichongli.com/api/";
+        public const string get_postsUrl = "get_posts/?read_more=More&count=10&page={0}";
+        public const string get_recent_postsUrl = "get_recent_posts/?read_more=More&count=10&page={0}";
+
+        public static Task<string> RequestAwait(string url)
+        {
+            return Task.Run(async () =>
+            {
+                string json = string.Empty;
+                HttpWebResponse response = null;
+                try
+                {
+                    HttpWebRequest request = HttpWebRequest.Create(baseUrl + url) as HttpWebRequest;
+
+                    response = (HttpWebResponse)await Task.Factory.FromAsync<WebResponse>(request.BeginGetResponse, request.EndGetResponse, TaskCreationOptions.None);
+                    using (var stream = response.GetResponseStream())
+                    {
+                        using (var reader = new StreamReader(stream))
+                        {
+                            json = await reader.ReadToEndAsync();
+                        }
+                    }
+                    if (response != null)
+                        response.Close();
+
+                }
+                catch
+                {
+                    json = null;
+                }
+                return json;
+            });
+        }
+    }
+}
---
+++ Ichongli.Rosi/NinjectBootStrapper.cs
@@ -0,0 +1,50 @@
+﻿using Caliburn.Micro;
+using Ichongli.Rosi.Services;
+using Microsoft.Phone.Shell;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ichongli.Rosi
+{
+    public partial class NinjectBootStrapper : PhoneBootstrapperBase
+    {
+        private IKernel _kernel;
+        
+        protected override void Configure()
+        {
+            this.ConfigureContainer();
+        }
+
+        private void ConfigureContainer()
+        {
+            this._kernel = new StandardKernel();
+
+            this._kernel.Bind<IWindowManager>().To<WindowManager>().InSingletonScope();
+            this._kernel.Bind<INavigationService>().ToConstant(new FrameAdapter(RootFrame)).InSingletonScope();
+            this._kernel.Bind<IEventAggregator>().To<EventAggregator>().InSingletonScope();
+            this._kernel.Bind<IPhoneService>().ToConstant(new PhoneApplicationServiceAdapter(PhoneApplicationService.Current, RootFrame)).InSingletonScope();
+            this._kernel.Bind<ILog>().ToMethod(context => LogManager.GetLog(context.Request.Target == null ? typeof(ILog) : context.Request.Target.Type));
+            this._kernel.Bind<IPostService>().To<PostService>();
+
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return this._kernel.Get(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return this._kernel.GetAll(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            this._kernel.Inject(instance);
+        }
+    }
+}
